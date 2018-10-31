d1f7fc7fe372e9c12dc70fdae79335bd7c48001d
Lizhen Shi <lizhen9.shi@gmail.com>
2018-10-31 11:16:05 -0400

    add Result class

--- python/src/gct/alg/clustering.py
+++ python/src/gct/alg/clustering.py
@@ -6,18 +6,97 @@ Created on Oct 27, 2018
 from gct import utils, config
 import os
 import json
-    
+import collections
+import pandas as pd     
+
     
 def load_rusult(dataname, algname):
     fpath = os.path.join(config.get_result_file_path(dataname, algname), 'result.txt')
     with open(fpath, 'rt') as f :
-        return json.load(f)
+        return Result(json.load(f))
 
 
 def save_result(result):
-    fpath = os.path.join(config.get_result_file_path(result['dataname'], result['algname']), 'result.txt')
-    with open(fpath, 'wt') as f:
-        json.dump(result, f)
+    if isinstance(result, Result): 
+        result.save()
+    else:
+        fpath = os.path.join(config.get_result_file_path(result['dataname'], result['algname']), 'result.txt')
+        with open(fpath, 'wt') as f:
+            json.dump(result, f)
+
+
+class Result(collections.MutableMapping):
+
+    def __init__(self, result):
+        if isinstance(result, Result):
+            self.store = result.store
+        elif isinstance(result, dict):
+            self.store = result 
+        else:
+            raise Exception("failed")
+        
+    def __getitem__(self, key):
+        return self.store[self.__keytransform__(key)]
+
+    def __setitem__(self, key, value):
+        self.store[self.__keytransform__(key)] = value
+
+    def __delitem__(self, key):
+        del self.store[self.__keytransform__(key)]
+
+    def __iter__(self):
+        return iter(self.store)
+
+    def __len__(self):
+        return len(self.store)
+
+    def __keytransform__(self, key):
+        return key
+
+    def __str__(self):
+        return str(self.store) 
+    
+    def __repr__(self):
+        return str(self.store)
+            
+    def save(self):
+        save_result(self.store)
+        return self 
+        
+    @property
+    def result(self): 
+        return self.store
+
+    def clusters(self, as_dataframe=False):
+        if as_dataframe:
+            d = self.get('clusters')
+            lst = [] 
+            for k, vs in d.items():
+                for v in vs: 
+                    lst.append([v, k])
+            return pd.DataFrame(lst, columns=['node', 'cluster'])
+        else:
+            return self.get('clusters')
+        
+    @property     
+    def algname(self):
+        return self.get("algname")
+
+    @property     
+    def params(self):
+        return self.get("params")
+
+    @property             
+    def dataname(self):
+        return self.get("dataname")
+
+    @property       
+    def meta(self):
+        return self.get("meta", None)
+
+    @property     
+    def timecost(self):
+        return self.get("timecost")        
 
 
 class Clustering(object):
@@ -35,7 +114,7 @@ class Clustering(object):
         
     def get_result(self):
         if hasattr(self, 'result'):
-            return self.result
+            return Result(self.result)
         else:
             raise Exception("No result found. probably no run has been done")
                 
---
+++ python/test/gct/alg/ClusteringTest.py
@@ -0,0 +1,51 @@
+'''
+Created on Oct 27, 2018
+
+@author: lizhen
+'''
+import unittest
+from gct.dataset import random_dataset
+from gct.alg import clustering
+from gct.alg.cggc_clustering import CGGC
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.graph_unweighted_undirect = random_dataset.generate_LFR("test_LFR_unw_und", N=128, k=16, maxk=16, muw=0.1, minc=32, beta=1, a=0)
+        assert not self.graph_unweighted_undirect.is_directed()
+        assert not self.graph_unweighted_undirect.is_weighted()
+        
+        self.graph_weighted_undirect = random_dataset.generate_LFR("test_LFR_w_und", N=128, k=16, maxk=16, muw=0.1, minc=32, beta=1, weighted=True, a=0)
+        assert not self.graph_weighted_undirect.is_directed()
+        assert self.graph_weighted_undirect.is_weighted()
+        
+        self.graph_weighted_direct = random_dataset.generate_LFR("test_LFR_w_dir", N=128, k=16, maxk=16, muw=0.1, minc=32, beta=1, weighted=True, a=1)
+        assert  self.graph_weighted_direct.is_directed()
+        assert self.graph_weighted_direct.is_weighted()
+        
+        self.graph_unweighted_direct = random_dataset.generate_LFR("test_LFR_unw_dir", N=128, k=16, maxk=16, muw=0.1, minc=32, beta=1, weighted=False, a=1)
+        assert  self.graph_unweighted_direct.is_directed()
+        assert not self.graph_unweighted_direct.is_weighted()
+        
+        self.graphs = [self.graph_unweighted_undirect, self.graph_weighted_undirect,
+                       self.graph_weighted_direct, self.graph_unweighted_direct]
+
+    def tearDown(self):
+        pass
+
+    def testResult(self):
+        alg = CGGC()
+        result = alg.run(self.graph_unweighted_undirect).get_result()
+        print (result.algname)
+        print (result.dataname)
+        print (result.meta)
+        print (result.params) 
+        print (result.timecost)
+        print (result.clusters())
+        print (result.clusters(as_dataframe=True))
+ 
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.testClauset_Newman_Moore']
+    unittest.main()
