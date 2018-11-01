4c5de538f8dd5c1ceb7abfca46eb6af2946d473a
Lizhen Shi <lizhen9.shi@gmail.com>
2018-11-01 13:34:24 -0400

    fix

--- python/src/gct/dataset/properties.py
+++ python/src/gct/dataset/properties.py
@@ -8,20 +8,21 @@ import numpy as np
 import pandas as pd   
 from gct.alg.clustering import Result
 import sys
+from gct.dataset import convert
 
 
 class GraphProperties(object):
 
     def __init__(self, data):
         if data.is_directed() or data.is_weighted():
-            print ("Warning! Graph will be taken as undirected and unweighted.")
+            print ("Warning! Graph will be taken as undirected.")
         self.data = data 
 
     def set_if_not_exists(self, name, fun):
         if hasattr(self, name):
             return getattr(self, name)
         else:
-            print ("call fun for " + name)
+            # print ("call fun for " + name)
             value = fun()
             setattr(self, name, value)
             return value 
@@ -32,7 +33,14 @@ class GraphProperties(object):
     
     @property
     def edges(self):
-        return self.set_if_not_exists("_edges", lambda: self.data.get_edges())
+
+        def f():
+            df = self.data.get_edges()
+            if not self.data.is_weighted():
+                df['weight'] = float(1)
+            return df 
+
+        return self.set_if_not_exists("_edges", f)
     
     @property
     def weighted(self):
@@ -60,11 +68,28 @@ class GraphProperties(object):
     
     @property
     def degrees(self):
+        return self.weighted_degrees()
+    
+    @property
+    def unweighted_degrees(self):
 
         def f():
             arr = self.edges[['src', 'dest']].values.ravel() 
             unique, counts = np.unique(arr, return_counts=True)
             return dict(zip(unique, counts))
+
+        prop_name = "_" + sys._getframe().f_code.co_name        
+        return self.set_if_not_exists(prop_name, f)
+
+    @property
+    def weighted_degrees(self):
+
+        def f():
+            df1 = self.edges[['src', 'weight']]
+            df2 = self.edges[['dest', 'weight']]
+            df2.columns = df1.columns
+            df = pd.concat([df1, df2])
+            return df.groupby('src')['weight'].sum().to_dict()
 
         prop_name = "_" + sys._getframe().f_code.co_name        
         return self.set_if_not_exists(prop_name, f)
@@ -87,15 +112,22 @@ class GraphClustersProperties(object):
         if hasattr(self, name):
             return getattr(self, name)
         else:
-            print ("call fun for " + name)
+            # print ("call fun for " + name)
             value = fun()
             setattr(self, name, value)
             return value 
     
     @property
     def edges(self):
+
+        def f():
+            df = self.data.get_edges()
+            if not self.data.is_weighted():
+                df['weight'] = float(1)
+            return df         
+
         prop_name = "_" + sys._getframe().f_code.co_name 
-        return self.set_if_not_exists(prop_name, lambda: self.data.get_edges())
+        return self.set_if_not_exists(prop_name, f)
     
     @property 
     def num_edges(self):
@@ -105,6 +137,38 @@ class GraphClustersProperties(object):
     def num_vectices(self):
         prop_name = "_" + sys._getframe().f_code.co_name        
         return self.set_if_not_exists(prop_name, lambda: len(set(np.unique(self.edges[['src', 'dest']].values))))
+    
+    @property
+    def degrees(self):
+        return self.weighted_degrees
+
+    @property
+    def sum_degree(self):
+        return np.sum(list(self.degrees.values()))
+        
+    @property
+    def unweighted_degrees(self):
+
+        def f():
+            arr = self.edges[['src', 'dest']].values.ravel() 
+            unique, counts = np.unique(arr, return_counts=True)
+            return dict(zip(unique, counts))
+
+        prop_name = "_" + sys._getframe().f_code.co_name        
+        return self.set_if_not_exists(prop_name, f)
+
+    @property
+    def weighted_degrees(self):
+
+        def f():
+            df1 = self.edges[['src', 'weight']]
+            df2 = self.edges[['dest', 'weight']]
+            df2.columns = df1.columns
+            df = pd.concat([df1, df2])
+            return df.groupby('src')['weight'].sum().to_dict()
+
+        prop_name = "_" + sys._getframe().f_code.co_name        
+        return self.set_if_not_exists(prop_name, f)
 
     @property 
     def num_clusters(self):
@@ -112,15 +176,43 @@ class GraphClustersProperties(object):
     
     @property 
     def cluser_indexes(self):
-        # prop_name = "_" + sys._getframe().f_code.co_name        
-        # return self.set_if_not_exists(prop_name, lambda: list(sorted((set(np.unique(self.clusters['cluster'].values))))))
         return self.cluser_sizes.keys()    
     
     @property 
     def cluser_sizes(self):
         prop_name = "_" + sys._getframe().f_code.co_name        
         return self.set_if_not_exists(prop_name, lambda: self.clusters[['node', 'cluster']].groupby('cluster')['node'].count().to_dict())
+    
+    @property 
+    def cluser_sum_intra_weights(self):
 
+        def f():
+            df = self.edges[['src', 'dest', 'weight']]
+            c_df = self.clusters[['node', 'cluster']].set_index('node')['cluster'].to_dict()
+            df['src_c'] = df['src'].map(c_df) 
+            df['dest_c'] = df['dest'].map(c_df)
+            df = df [(df['src_c'] == df['dest_c'])]
+
+            a = df[['src_c', 'weight']].groupby('src_c')['weight'].sum() * 2
+            return a.to_dict()
+                
+        prop_name = "_" + sys._getframe().f_code.co_name
+        
+        return self.set_if_not_exists(prop_name, f)        
+
+    @property 
+    def cluser_sum_weighted_degrees(self):
+
+        def f():
+            df = self.clusters[['node', 'cluster']]
+            d = self.weighted_degrees
+            df['wd'] = df['node'].map(d)
+            return df.groupby("cluster")['wd'].sum().to_dict()
+                
+        prop_name = "_" + sys._getframe().f_code.co_name
+        
+        return self.set_if_not_exists(prop_name, f)        
+        
     @property
     def cluser_edge_sizes(self):
 
@@ -140,6 +232,12 @@ class GraphClustersProperties(object):
     
     @property
     def intra_cluster_densities(self):
+        if self.data.is_weighted():
+            raise Exception("only support unweighted graph")
+        return self.unweighted_intra_cluster_densities
+    
+    @property
+    def unweighted_intra_cluster_densities(self):
 
         def f():
             r = {}
@@ -157,12 +255,83 @@ class GraphClustersProperties(object):
     @property
     def intra_cluster_density(self):
         return np.mean(list(self.intra_cluster_densities.values()))    
-    
+
     @property
     def inter_cluster_density(self):
+        if self.data.is_weighted():
+            raise Exception("only support unweighted graph")        
+        return self.inter_unweighted_cluster_density
+    
+    @property
+    def inter_unweighted_cluster_density(self):
         n_intra_edges = np.sum(list(self.cluser_edge_sizes.values()))
         n_edge = self.num_edges
         n = self.num_vectices
         d = n * (n - 1) - np.sum([u * (u - 1) for u in self.cluser_sizes.values()])
         return float(n_edge - n_intra_edges) * 2 / d
+
+    @property
+    def relative_cluser_densities(self):
+
+        def f():
+            df = self.edges[['src', 'dest', 'weight']]
+            c_df = self.clusters[['node', 'cluster']].set_index('node')['cluster'].to_dict()
+            df['src_c'] = df['src'].map(c_df) 
+            df['dest_c'] = df['dest'].map(c_df)
+            df['is_intra'] = (df['src_c'] == df['dest_c'])
+            df['is_inter'] = (df['src_c'] != df['dest_c'])
+
+            a = df[df['is_intra']].groupby('src_c')['weight'].sum()
+            b = df[df['is_inter']].groupby('src_c')['weight'].sum()
+            c = df[df['is_inter']].groupby('dest_c')['weight'].sum()
+
+            newdf = pd.concat([a, b, c], axis=1).fillna(0)
+            newdf.columns = ['intra', 'inter1', 'inter2']
+            newdf['density'] = newdf['intra'] / (newdf['inter1'] + newdf['inter2'] + newdf['intra'])
+            
+            return newdf['density'].to_dict()
+                
+        prop_name = "_" + sys._getframe().f_code.co_name
+        
+        return self.set_if_not_exists(prop_name, f)       
+
+    @property
+    def snap_modularities(self):
+        import snap
+
+        def f():
+            G = convert.to_snap(self.data)
+            m = self.num_edges
+            ret = {}
+            for k, v in self.clusters.groupby('cluster')['node'].apply(lambda u: list(u)).to_dict().items():
+                Nodes = snap.TIntV()
+                for nodeId in v:
+                    Nodes.Add(nodeId)
+                    print (nodeId, m)
+                mod = snap.GetModularity(G, Nodes, 1024)
+                ret[k] = mod 
+            return ret 
+
+        prop_name = "_" + sys._getframe().f_code.co_name
+        
+        return self.set_if_not_exists(prop_name, f)       
+
+    @property
+    def modularity(self):
+        return self.modularity2
+
+    @property
+    def modularity2(self):  # another formula
+
+        def f():
+            c = self.sum_degree
+            d = 1.0 / (c * c)
+            a = np.sum(list(self.cluser_sum_intra_weights.values())) / c
+            b = np.sum([u * u * d for u in self.cluser_sum_weighted_degrees.values()])
+            print (self.num_edges, a, b, c, d)
+            return a - b
+            
+        prop_name = "_" + sys._getframe().f_code.co_name
+        
+        return self.set_if_not_exists(prop_name, f)       
     
--- python/test/gct/dataset/propertiesTest.py
+++ python/test/gct/dataset/propertiesTest.py
@@ -11,34 +11,44 @@ from gct.dataset.properties import GraphProperties, GraphClustersProperties
 class Test(unittest.TestCase):
 
     def setUp(self):
-        self.graph_unweighted_undirect = random_dataset.generate_LFR("test_LFR_unw_und", N=128, k=16, maxk=16, muw=0.1, minc=32, beta=1,a=0)
+        self.graph_unweighted_undirect = random_dataset.generate_LFR("test_LFR_unw_und", N=128, k=16, maxk=16, muw=0.1, minc=32, beta=1, a=0)
         assert not self.graph_unweighted_undirect.is_directed()
         assert not self.graph_unweighted_undirect.is_weighted()
-        
 
     def tearDown(self):
         pass
 
     def testGraphProperties(self):
         p = GraphProperties(self.graph_unweighted_undirect)
-        print (p.num_edges)
-        print (p.num_vectices)
-        print (p.density)
-        print (p.density1)
-        print (p.degrees)
+        print ('num_edges', p.num_edges)
+        print ('num_vectices', p.num_vectices)
+        print ('density', p.density)
+        print ('density1', p.density1)
+        print ('weighted_degrees', p.weighted_degrees)
+        print ('unweighted_degrees', p.unweighted_degrees)
 
     def testClustersProperties(self):
-        g=self.graph_unweighted_undirect
-        p = GraphClustersProperties(g,g.get_ground_truth())
-        print (p.num_edges)
-        print (p.num_vectices)
-        print (p.num_clusters)
-        print (p.cluser_sizes)
-        print (p.cluser_indexes)
-        print (p.cluser_edge_sizes)
-        print (p.intra_cluster_densities)
-        print (p.intra_cluster_density)
-        print (p.inter_cluster_density)
+        g = self.graph_unweighted_undirect
+        assert not g.is_weighted()
+        p = GraphClustersProperties(g, g.get_ground_truth())
+        print ('num_edges', p.num_edges)
+        print ('num_vectices', p.num_vectices)
+        print ('num_clusters', p.num_clusters)
+        print ('cluser_sizes', p.cluser_sizes)
+        print ('cluser_indexes', p.cluser_indexes)
+        print ('cluser_edge_sizes', p.cluser_edge_sizes)
+        print ('cluser_sum_intra_weights',p.cluser_sum_intra_weights)
+        print ('unweighted_intra_cluster_densities', p.unweighted_intra_cluster_densities)
+        print ('cluser_sum_weighted_degrees', p.cluser_sum_weighted_degrees)
+        
+        print ('intra_cluster_densities', p.intra_cluster_densities)
+        print ('intra_cluster_density', p.intra_cluster_density)
+        print ('inter_cluster_density', p.inter_cluster_density)
+        print ('relative_cluser_densities', p.relative_cluser_densities)
+        print ("modularity1", p.modularity1)
+        print ("modularity2", p.modularity2)
+        # print ("snap_modularities", p.snap_modularities)        
+        # print ("snap_modularity", p.snap_modularity)
         
                                     
 if __name__ == "__main__":
