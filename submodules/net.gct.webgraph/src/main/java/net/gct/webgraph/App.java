e9bdde202d9197cce1ee79531709e69b86d5f71a
Lizhen Shi <lizhen9.shi@gmail.com>
2018-11-14 05:50:37 -0500

    add webgrap dep

---
+++ submodules/net.gct.webgraph/src/main/java/net/gct/webgraph/App.java
@@ -0,0 +1,23 @@
+package net.gct.webgraph;
+
+import it.unimi.dsi.webgraph.ArcListASCIIGraph;
+import it.unimi.dsi.webgraph.BVGraph;
+import it.unimi.dsi.webgraph.algo.ApproximateNeighbourhoodFunctions;
+import it.unimi.dsi.webgraph.algo.ConnectedComponents;
+import it.unimi.dsi.webgraph.algo.HyperBall;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ArcListASCIIGraph a;
+    	BVGraph b;
+    	ApproximateNeighbourhoodFunctions c;
+    	HyperBall d;
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ submodules/net.gct.webgraph/src/main/java/net/gct/webgraph/EdgeListGraph.java
@@ -0,0 +1,17 @@
+package net.gct.webgraph;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import it.unimi.dsi.webgraph.ImmutableSequentialGraph;
+
+public class EdgeListGraph extends ImmutableSequentialGraph {
+	private final static boolean DEBUG = false;
+	private static final Logger LOGGER = LoggerFactory.getLogger(EdgeListGraph.class);
+	
+	@Override
+	public int numNodes() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
